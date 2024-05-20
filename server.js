1c1d872c740db1afe8dc9bf23e32c0315f860387
Matt <mphlamb@gmail.com>
2024-05-20 09:57:26 +0200

    functions updated

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require("method-override")
+const morgan = require("morgan")
+
+
 // ? REQUIRE MODEL
 const Moss = require('./models/moss')
 const port = 3000
@@ -10,8 +14,9 @@ const app = express()
 const path = require("path");
 
 
-
-
+app.use(methodOverride("_method"));
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }));
 mongoose.connect(process.env.MONGODB_URI)
 
 
@@ -19,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 
-// * PAGE RENDER ---------------------------------
+// * PAGE RENDER -----------------------------------------
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -48,11 +53,26 @@ app.get('/post', (req, res) => {
 })
 
 
+app.get('/mosses/:id/edit', async (req, res) => {
+    const id = req.params.id
+    const moss = await Moss.findById(id)
+
+    res.render(id/'edit.ejs', {
+        moss
+    })
+})
+
+
+
+
+
+
+
 app.use(express.json())
 
-// * ---------------------------------------------
+// * ----------------------------------------------------
 app.use(express.urlencoded({ extended: false}))
-// * ---------------------------------------------
+// * ----------------------------------------------------
 
 
 // ! POST USING FORM
@@ -74,7 +94,7 @@ app.delete('/mosses/:mossId', async (req, res) => {
     const deletedMoss = await Moss.findByIdAndDelete(req.params.mossId)
 
     // ? SEND
-    res.send(deletedMoss)
+    res.redirect('/mosses/')
 
 })
 
@@ -82,12 +102,19 @@ app.delete('/mosses/:mossId', async (req, res) => {
 app.put('/mosses/:mossId', async (req, res) => {
 
     // ? ONE STEP
-    // ! < -- NEW: TRUE will render updated element in Postman
+    // ! below.. NEW: TRUE will render updated element
     const updatedMoss = await Moss.findByIdAndUpdate(req.params.mossId, req.body, { new: true })
 
     res.send(updatedMoss)
 })
 
+
+app.get('mosses/:mossId/edit', async (req, res) => {
+    const foundMoss = await Moss.findById(req.params.mossId)
+    res.render('/edit/', {
+        moss: foundMoss,
+    })
+})
 
 
 
