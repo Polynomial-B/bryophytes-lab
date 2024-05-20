2df64d8744d3436adf257f10702bf220ce7728ce
Matt <mphlamb@gmail.com>
2024-05-20 09:57:59 +0200

    Seed data added in separate file

---
+++ seedData.js
@@ -0,0 +1,153 @@
+const bryophytesArray = [
+
+    {
+      commonName: "Springy turf-moss",
+      latinName: "Rhytidiadelphus squarrosus",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2021/01/2020-12-24-13-34-15.jpg"
+    },
+    {
+      commonName: "Pointed Spear-moss",
+      latinName: "Calliergonella cuspidata",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2020-12-29-15-25-43.jpg"
+    },
+    {
+      commonName: "Grey-cushioned Grimmia",
+      latinName: "Grimmia pulvinata",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2019-01-01-13-38-03.jpg"
+    },
+    {
+      commonName: "Dissimilar Grimmia",
+      latinName: "Grimmia dissimulata",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: ""
+    },
+    {
+      commonName: "Wall Screw-moss",
+      latinName: "Tortula muralis",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Tortula-muralis-with-capsules-1207-cropped.jpg"
+    },
+    {
+      commonName: "Intermediate Screw-moss",
+      latinName: "Syntrichia montana",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2011-11-05-10.14.09.jpg"
+    },
+    {
+      commonName: "Anomalous Bristle-moss",
+      latinName: "Orthotrichum anomalum",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2022/02/2022-02-11-13-52-21.jpg"
+    },
+    {
+      commonName: "Thickpoint Grimmia",
+      latinName: "Schistidium crassipilum",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2022/05/2022-03-20-13-21-51-BRadius3Smoothing1.jpg"
+    },
+    {
+      commonName: "Silky Wall Feather-moss",
+      latinName: "Homalothecium sericeum",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2020-08-15-11-55-19.jpg"
+    },
+    {
+      commonName: "Cypress-leaved Plait-moss",
+      latinName: "Hypnum cupressiforme",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2015-01-23-13-28-54-9.jpg"
+    },
+    {
+      commonName: "Slender Mouse-tail Moss",
+      latinName: "Isothecium myosuroides",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Isothecium-myosuroides-1-305.jpg"
+    },
+    {
+      commonName: "Wood Bristle-moss",
+      latinName: "Lewinskya affinis",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2017-01-22-13-54-22.jpg"
+    },
+    {
+      commonName: "Lateral Cryphaea",
+      latinName: "Cryphaea heteromalla",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2021/03/2021-03-15-16-40-54.jpg"
+    },
+    {
+      commonName: "Common Striated Feather-moss",
+      latinName: "Eurhynchium striatum",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Eurhynchium-striatum-305.jpg"
+    },
+    {
+      commonName: "Fox-tail Feather-moss",
+      latinName: "Thamnobryum alopecurum",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Thamnobryum-alopecurum-1104.jpg"
+    },
+    {
+      commonName: "Common Tamarisk-moss",
+      latinName: "Thuidium tamariscinum",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Thuidium-tamariscinum-305.jpg"
+    },
+    {
+      commonName: "Rough-stalked Feather-moss",
+      latinName: "Brachythecium rutabulum",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Brachythecium-rutabulum-0620.jpg"
+    },
+    {
+      commonName: "Common Feather-moss",
+      latinName: "Kindbergia praelonga",
+      division: "Moss",
+      growthForm: "Pleurocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2014-10-10-13.34.59.jpg"
+    },
+    {
+      commonName: "Bank Haircap",
+      latinName: "Polytrichum formosum",
+      division: "Moss",
+      growthForm: "Acrocarp",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Polytrichastrum-formosum-0512.jpg"
+    },
+    {
+      commonName: "Dilated Scalewort",
+      latinName: "Frullania dilatata",
+      division: "Liverwort",
+      growthForm: "Leafy",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/2020-10-01-15-24-54.jpg"
+    },
+    {
+      commonName: "Forked Veilwort",
+      latinName: "Metzgeria furcata",
+      division: "Liverwort",
+      growthForm: "Thalloid",
+      image: "https://www.britishbryologicalsociety.org.uk/wp-content/uploads/2020/12/Metzgeria-furcata-0215.jpg"
+    },
+  ];
+  
+  
+module.exports = bryophytesArray
